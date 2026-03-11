df5f8d63ecff981aec6499284a638c006b1822a8
Natte Vagon <nattevagon@gmail.com>
2026-03-11 18:31:47 +0700

    feat(init): revert motion transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css'
+import Home from "./pages/Home";
+import Lists from "./pages/Lists";
 import Navigation from "./components/molecules/Navigation";
+import Reports from "./pages/Reports";
+import FormDebtTransaction from "./pages/FormDebtTransaction";
+import FormPocketTransaction from "./pages/FormPocketTransaction";
+import ChooseFormTypeTransaction from "./components/molecules/Modal/ChooseTypeTransaction";
+import ConfirmFinishDebt from "./components/molecules/Modal/ConfirmFinishDebt";
 import { ModalProvider } from "./context/ModalContext";
 import GlobalModal from "./components/molecules/Modal/GlobalModal";
-import AnimateRoute from "./routes/AnimateRoute";
-import './App.css'
+import PersonDebt from "./pages/PersonDebt";
+import Accounts from "./pages/Accounts";
+import FormAccount from "./pages/FormAccount";
 
 function App() {
   return (
     <BrowserRouter>
       <ModalProvider>
-        <div className="flex flex-col h-screen bg-[#141414] overflow-hidden">
-          <main className="flex-1 overflow-y-auto pb-20">
-            <AnimateRoute />
-          </main>
-          <Navigation />
-        </div>
+        <main className="bg-[#141414] flex flex-col min-h-screen pb-20">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/lists" element={<Lists />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/debt-transaction/:type/:id?" element={<FormDebtTransaction />} />
+            <Route path="/pocket-transaction/:type/:id?" element={<FormPocketTransaction />} />
+            <Route path="/person-debt/:id" element={<PersonDebt />} />
+            <Route path="/account" element={<Accounts />} />
+            <Route path="/account/:type/:id?" element={<FormAccount />} />
+          </Routes>
+        </main>
+        <Navigation />
         <GlobalModal />
       </ModalProvider>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default App
