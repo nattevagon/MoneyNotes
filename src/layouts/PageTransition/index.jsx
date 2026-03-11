897d65b9b83ebed330560665c112d5248a81a562
Natte Vagon <nattevagon@gmail.com>
2026-03-11 18:19:16 +0700

    Revert "feat(init): fixing motion transition"
    This reverts commit cc70aa389e467fd6939a5f9c1b5f372f1a7cf840.

--- src/layouts/PageTransition/index.jsx
+++ src/layouts/PageTransition/index.jsx
@@ -3,14 +3,13 @@ import { motion } from "framer-motion";
 export default function PageTransition({ children }) {
   return (
     <motion.div
-      className="transform-gpu will-change-transform"
-      style={{ transform: "translateZ(0)" }}
-      initial={{ x: 70, opacity: 0 }}
+      className="will-change-transform"
+      initial={{ x: 50, opacity: 0 }}
       animate={{ x: 0, opacity: 1 }}
-      exit={{ x: -70, opacity: 0 }}
+      exit={{ x: -50, opacity: 0 }}
       transition={{
         x: {
-          duration: 0.22,
+          duration: 0.18,
           ease: [0.25, 1, 0.5, 1]
         },
         opacity: {
